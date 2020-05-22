eb68562bc42bfb5f95b0c9c6f735bce4171ff916
Ahmed Elmaghallawy <ahmedradwan9966@gmail.com>
2020-05-22 15:18:48 +0200

    ues form for dynamic greeting

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template
 from flask_moment import Moment
-
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'string used as a secret key'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
 
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
