d3326c68fc4b7237611bc824a83c27f70f52b550
Ahmed Elmaghallawy <ahmedradwan9966@gmail.com>
2020-05-30 20:09:12 +0200

    set password and hash it for User Model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Role(db.Model):
+    """Role model"""
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
@@ -12,10 +14,26 @@ class Role(db.Model):
 
 
 class User(db.Model):
+    """User model"""
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        """raise an error when trying to get the password"""
+        raise AttributeError('password is not readable attribute')
+
+    @password.setter
+    def password(self, password):
+        """generate hash value for a given password"""
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """verify a given password"""
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %r>' % self.username
