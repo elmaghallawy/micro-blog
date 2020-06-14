aaa1bf52472a56494164598da6216a5e46307e9d
Ahmed Elmaghallawy <ahmedradwan9966@gmail.com>
2020-06-14 18:46:07 +0200

    add views for moderators and admins

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,24 @@
 from flask import render_template, session, redirect, url_for, current_app
-from .. import db
-from ..models import User
-from ..email import send_email
 from . import main
-from .forms import NameForm
+from flask_login import login_required
+from app.decorators import permission_required, admin_required
+from ..models import Permission
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    
     return render_template('index.html')
+
+
+@main.route('/admin')
+@login_required
+@admin_required
+def for_admins_only():
+    return 'for administrators only!'
+
+
+@main.route('/moderate')
+@login_required
+@permission_required(Permission.MODERATE)
+def for_moderators_only():
+    return 'for comment moderators only'
