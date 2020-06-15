383a65b680459b7c27d6b214559247f32fa3d2db
Ahmed Elmaghallawy <ahmedradwan9966@gmail.com>
2020-06-15 17:23:26 +0200

    add user profile

--- app/main/views.py
+++ app/main/views.py
@@ -2,8 +2,7 @@ from flask import render_template, session, redirect, url_for, current_app
 from . import main
 from flask_login import login_required
 from app.decorators import permission_required, admin_required
-from ..models import Permission
-
+from ..models import Permission, User
 
 @main.route('/')
 def index():
@@ -22,3 +21,9 @@ def for_admins_only():
 @permission_required(Permission.MODERATE)
 def for_moderators_only():
     return 'for comment moderators only'
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
+
