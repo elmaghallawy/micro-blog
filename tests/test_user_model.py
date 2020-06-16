377dd595d34d9b64486bdac2fab7f1b4d8b6fa61
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-06-16 20:54:30 +0200

    test permissions

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,18 +1,17 @@
 import unittest
 import time
-from app.models import User
+from datetime import datetime
 from app import create_app, db
+from app.models import User, AnonymousUser, Role, Permission
 
 
 class UserModelTestCase(unittest.TestCase):
-    """class for User model Tests"""
-
     def setUp(self):
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
-        self.user = User(password='cat')
+        Role.insert_roles()
 
     def tearDown(self):
         db.session.remove()
@@ -20,41 +19,98 @@ class UserModelTestCase(unittest.TestCase):
         self.app_context.pop()
 
     def test_password_setter(self):
-        """test setting the password and generating
-        hash for it"""
-
-        self.assertTrue(self.user.password_hash is not None)
+        u = User(password='cat')
+        self.assertTrue(u.password_hash is not None)
 
     def test_no_password_getter(self):
-        """test that password cannot be read"""
+        u = User(password='cat')
         with self.assertRaises(AttributeError):
-            self.user.password
+            u.password
 
     def test_password_verification(self):
-        """test password verification works"""
-        self.assertTrue(self.user.verify_password('cat'))
-        self.assertFalse(self.user.verify_password('dog'))
+        u = User(password='cat')
+        self.assertTrue(u.verify_password('cat'))
+        self.assertFalse(u.verify_password('dog'))
 
     def test_password_salts_are_random(self):
+        u = User(password='cat')
         u2 = User(password='cat')
-        self.assertTrue(self.user.password_hash != u2.password_hash)
+        self.assertTrue(u.password_hash != u2.password_hash)
 
-    def test_confirmation_valid_token(self):
-        """test valid confirmation token"""
-        token = self.user.generate_confirmation_token()
-        self.assertTrue(self.user.confirm(token))
+    def test_valid_confirmation_token(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token()
+        self.assertTrue(u.confirm(token))
 
-    def test_confirmation_invalid_token(self):
-        """test invalid confirmation token"""
+    def test_invalid_confirmation_token(self):
+        u1 = User(password='cat')
         u2 = User(password='dog')
-        db.session.add(self.user)
+        db.session.add(u1)
         db.session.add(u2)
         db.session.commit()
-        token = self.user.generate_confirmation_token()
+        token = u1.generate_confirmation_token()
         self.assertFalse(u2.confirm(token))
 
     def test_expired_confirmation_token(self):
-        """test expired confirmation token"""
-        token = self.user.generate_confirmation_token(1)
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token(1)
         time.sleep(2)
-        self.assertFalse(self.user.confirm(token))
+        self.assertFalse(u.confirm(token))
+
+
+
+    def test_user_role(self):
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.can(Permission.FOLLOW))
+        self.assertTrue(u.can(Permission.COMMENT))
+        self.assertTrue(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
+
+    def test_moderator_role(self):
+        r = Role.query.filter_by(name='Moderator').first()
+        u = User(email='john@example.com', password='cat', role=r)
+        self.assertTrue(u.can(Permission.FOLLOW))
+        self.assertTrue(u.can(Permission.COMMENT))
+        self.assertTrue(u.can(Permission.WRITE))
+        self.assertTrue(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
+
+    def test_administrator_role(self):
+        r = Role.query.filter_by(name='Administrator').first()
+        u = User(email='john@example.com', password='cat', role=r)
+        self.assertTrue(u.can(Permission.FOLLOW))
+        self.assertTrue(u.can(Permission.COMMENT))
+        self.assertTrue(u.can(Permission.WRITE))
+        self.assertTrue(u.can(Permission.MODERATE))
+        self.assertTrue(u.can(Permission.ADMIN))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
+        self.assertFalse(u.can(Permission.COMMENT))
+        self.assertFalse(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
+
+    def test_timestamps(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        self.assertTrue(
+            (datetime.utcnow() - u.member_since).total_seconds() < 3)
+        self.assertTrue(
+            (datetime.utcnow() - u.last_seen).total_seconds() < 3)
+
+    def test_ping(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        time.sleep(2)
+        last_seen_before = u.last_seen
+        u.ping()
+        self.assertTrue(u.last_seen > last_seen_before)
