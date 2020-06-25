0e149a44c0432ad8fc22341236a91359a2f9357c
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-06-25 21:42:33 +0200

    handle rich text on server

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ from flask_login import UserMixin, AnonymousUserMixin
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app, request
+from markdown import markdown
+import bleach
 
 
 class Permission:
@@ -185,3 +187,15 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)
+
+    @staticmethod
+    def on_changed_body(target, value, old_value, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True
+        ))
+        db.event.listen(Post.body, 'set', Post.on_changed_body)
