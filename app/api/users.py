cbd24976d5542e322c27c5218bb987bd487e7ea2
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-07-20 22:45:11 +0200

    add users endpoints

---
+++ app/api/users.py
@@ -0,0 +1,20 @@
+from flask import g, jsonify, request, url_for
+from ..models import User, db
+
+
+@api.route('/users/<int:id>')
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return jsonify(user.to_json())
+
+
+@api.route('users/<int:id>/posts/')
+def get_user_posts(id):
+    user = User.query.get_or_404(id)
+    return jsonify({'posts': [post.to_json() for post in user.posts]})
+
+
+@api.route('posts/<int:id>/timeline/')
+def get_user_followed_posts(id):
+    user = User.query.get_or_404(id)
+    return jsonify({'posts': [post.to_json() for post in user.followed_posts]})
