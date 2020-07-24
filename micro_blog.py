7be5c6f01c93ebf8bf3df0d2bfd5c93a5daf095f
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-07-24 10:56:27 +0200

    add app profiling command

--- micro_blog.py
+++ micro_blog.py
@@ -50,3 +50,17 @@ def test(coverage):
         COV.html_report(directory=covdir)
         print('HTML version: file://%s/index.html' % covdir)
         COV.erase()
+
+
+@app.cli.command()
+@click.option('--length', default=25,
+              help='Number of functions to include in the profiler report.')
+@click.option('--profile-dir', default=None,
+              help='Directory where profiler data files are saved.')
+def profile(length, profile_dir):
+    """start the application under the code profiler."""
+    from werkzeug.middleware.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+    if __name__ == "__main__":
+        app.run(debug=False)
