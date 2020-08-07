b514a05e009ae1f1625665b5118001a110606f4a
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-08-07 09:33:26 +0200

    remove deprecated method

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField, ValidationError
-from wtforms.validators import DataRequired, Length, Email, Regexp, Required
+from wtforms.validators import DataRequired, Length, Email, Regexp
 from ..models import Role, User
 from flask_pagedown.fields import PageDownField
 
@@ -46,7 +46,7 @@ class EditProfileAdminForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
-    body = PageDownField('What\'s on your mind?', validators=[Required()])
+    body = PageDownField('What\'s on your mind?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
